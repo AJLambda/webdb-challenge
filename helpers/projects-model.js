2b762c8d78e84bf10800de16396aa30e85f234bd
AJLambda <apj2277@gmail.com>
2019-05-03 14:13:07 -0700

    working on get projects object structure

--- helpers/projects-model.js
+++ helpers/projects-model.js
@@ -12,16 +12,23 @@ function find() {
 }
 
 function findById(id) {
-  return db("projects").where({ "projects.id": id });
+  return db("projects")
+    .select(
+      "projects.id",
+      "projects.project_name as name",
+      "projects.project_description as description",
+      "projects.completed"
+    )
+    .where({ "projects.id": id });
 }
 
 function findProjectActions(projectId) {
   return db("actions")
     .select(
-      "actions.id as action_id",
-      "actions.actions_description as action_description",
-      "actions.action_notes",
-      "actions.completed as action_completed"
+      "actions.id as id",
+      "actions.actions_description as description",
+      "actions.action_notes as notes",
+      "actions.completed as completed"
     )
     .where({ project_id: projectId });
 }

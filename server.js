8485c30f180f9b0af940a0a88172954f62c05281
AJLambda <apj2277@gmail.com>
2019-05-03 09:28:17 -0700

    get and post actions working

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(parser);
 server.use(helmet());
 
 const Projects = require("./helpers/projects-model");
-// const Actions = require("./helpers/actions-model");
+const Actions = require("./helpers/actions-model");
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "hello" });
@@ -37,6 +37,34 @@ server.post("/api/projects", async (req, res) => {
     }
   } else {
     res.status(400).json({ message: "Please provide name of the project" });
+  }
+});
+
+// get all actions
+server.get("/api/actions", async (req, res) => {
+  try {
+    const actions = await Actions.find();
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving the actions" });
+  }
+});
+
+// post an action
+server.post("/api/actions", async (req, res) => {
+  const action = req.body;
+
+  if (action.actions_description && action.project_id) {
+    try {
+      const inserted = await Actions.add(action);
+      res.status(201).json(inserted);
+    } catch (error) {
+      res.status(500).json({ message: "Error creating the action" });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide name and ID of the action" });
   }
 });
 
